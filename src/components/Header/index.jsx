e8a7b3e943704cef188c78f2b98f72e638e33b06
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-10-23 08:42:44 -0300

    Efeito de menu de opçoes para o usuario

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,39 @@
 import Logo from "../../assets/netflix-logo.svg";
 import "./styles.css";
+import { useState } from "react";
 
 export const Header = ({ isBlack }) => {
+  const [settingsIsActive, setSettingsIsActive] = useState(false);
+  function handleOpenProfileSettings() {
+    setSettingsIsActive(!settingsIsActive);
+  }
+
   return (
-    <header className={isBlack ? 'black' : ''}>
+    <header className={isBlack ? "black" : ""}>
       <div className="header--logo">
         <a href="/">
           <img src={Logo} alt="Netflix" />
         </a>
       </div>
-      <div className="header--user">
-        <img
-          src="http://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-          alt="Perfil"
-        />
-      </div>
+      {!settingsIsActive && (
+        <div className="header--user" onClick={handleOpenProfileSettings}>
+          <img
+            src="http://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt="Perfil"
+          />
+        </div>
+      )}
+      {settingsIsActive && (
+        <div
+          className="header--settings"
+          onMouseLeave={handleOpenProfileSettings}
+        >
+          <span>Olá, usuário(a)!</span>
+          <span>Minha conta</span>
+          <span>Categorias</span>
+          <span>Configurações</span>
+        </div>
+      )}
     </header>
   );
 };
--- src/components/MovieRow/index.jsx
+++ src/components/MovieRow/index.jsx
@@ -25,7 +25,6 @@ export const MovieRow = ({ title, items }) => {
   };
 
   useEffect(() => {
-
   }, [items])
 
   return (
